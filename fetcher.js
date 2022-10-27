b5d20055f1fbd33c18e85e55d958bb126610aa33
Maris <mcclapperton@gmail.com>
2022-10-26 21:40:52 -0600

    first commit - cant get this to work in node, ~ an hour done

---
+++ fetcher.js
@@ -0,0 +1,30 @@
+
+const fs = require("fs");
+const request = require("request");
+// const { request } = require("http");
+
+const aURL = process.argv[2];
+const localPathToFile = process.argv[3];
+
+request(aURL, (error, response, body) => {
+  if (error) {
+    console.log("error:", error);
+  }
+  fs.writeFile(`${localPathToFile}`, body, (error) => {
+    if (error) {
+      console.log("error:", error);
+    } else {
+      console.log(
+        `Downloaded and saved ${response.headers} bytes to ${localPathToFile}`
+      );
+    }
+    // file written successfully
+  });
+  // make http request
+  //wait for resp
+  //when req complete take data received and write into local file sys
+  //  one character = 1 byte
+
+  // console.log("statusCode:", response && response.statusCode);
+  // console.log("body:", body);
+});
